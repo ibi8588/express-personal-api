5638cc482c166af6279406e39745f19fcf8c64a2
ibi8588 <ed8588@gmail.com>
2017-05-29 13:46:25 -0700

    routes working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,98 @@
 console.log("Sanity Check: JS is working!");
+// var express = require('express')
+// var app = express();
+// var bodyParser = require('body-parser');
 
-$(document).ready(function(){
 
-// your code
+$(document).ready(function() {
+  //var $playerlist
+  $.ajax({
+    //$playerlist = $('#playerTarget'),
+    method: 'GET',
+    url: '/api/players',
+    success: loadPlayers,
+    error: playerError
+  })
+
 
 });
+
+function loadPlayers(json) {
+  $('#playerTarget').append(`<tbody>
+    <tr>
+      <td rowspan="7"> <img id="imageURL" src="http://placehold.it/400x300" alt="Player" height="400" width="300"> </td>
+      <td>Player Name:</td>
+      <td>name="name"</td>
+    </tr>
+    <tr>
+      <td>Years Played:</td>
+       <td>name="yearsPlayed"&nbsp;</td>
+    </tr>
+    <tr>
+      <td>Team:</td>
+      <td>name="team"&nbsp;</td>
+    </tr>
+    <tr>
+      <td>Position:</td>
+      <td>name="position"&nbsp;</td>
+    </tr>
+    <tr>
+      <td>World Series Wins:</td>
+      <td>name="worldSeriesWins"&nbsp;</td>
+    </tr>
+    <tr>
+      <td>MVP:</td>
+      <td>name="mvp"&nbsp;</td>
+    </tr>
+    <tr>
+      <td>Statistics:</td>
+      <td>name="statistics"&nbsp;</td>
+      <div id="playerTarget">
+    </tr>
+  </tbody>`)
+
+}
+
+function playerError(e){
+  console.log('error.')
+}
+
+function validate() {
+  function errorMessage() {
+    alert("Must create valid entry.");
+  }
+  if (document.createNewPlayer.name.value == "") {
+    errorMessage();
+    document.createNewPlayer.name.focus();
+    return false;
+  }
+  if (document.createNewPlayer.yearsPlayed.value == 0) {
+    errorMessage();
+    document.createNewPlayer.yearsPlayed.focus();
+    return false;
+  }
+  if (document.createNewPlayer.team.value == "") {
+    errorMessage();
+    return false;
+  }
+  if (document.createNewPlayer.imageURL.value = "") {
+    errorMessage();
+    return false
+  }
+  if (document.createNewPlayer.position.value = "") {
+    errorMessage();
+    return false;
+  }
+  if (document.createNewPlayer.worldSeriesWins = "" || NaN) {
+    errorMessage();
+    return false;
+  }
+  if (document.createNewPlayer.mvp.value = "" || NaN) {
+    errorMessage();
+    return false;
+  }
+  if (document.createNewPlayer.statistics.value = "") {
+    errorMessage();
+    return false;
+  }
+}
--- server.js
+++ server.js
@@ -36,7 +36,119 @@ app.use(express.static('public'));
 app.get('/', function homepage(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
+//player list for server test.
+var players_list = [
+  {
+    name: "Ibrahim Aldridge",
+    yearsPlayed: 32,
+    team: "San Francisco Venture Capitalists",
+    imgURL: "https://lh3.googleusercontent.com/-yDMMcpIxaMQ/AAAAAAAAAAI/AAAAAAAAHbw/RCCjxJLrSz4/s640/photo.jpg",
+    position: "CF",
+    worldSeriesWins: 30,
+    mvp: 10,
+    stats: "http://baseball.zengm.com/l/2/player/1068"
+  },
+  {
+    name: "Donald Barney",
+    yearsPlayed: 17,
+    team: "San Francisco Venture Capitalists",
+    imgURL: "http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/28963.png&w=350&h=254",
+    position: "C",
+    worldSeriesWins: 14,
+    mvp: 5,
+    stats: "http://baseball.zengm.com/l/2/player/2281"
+  },
+  {
+    name: "Billy Brown",
+    yearsPlayed: 17,
+    team: "Miami Cyclones",
+    imgURL: "",
+    position: "LF",
+    worldSeriesWins: 0,
+    mvp: 0,
+    stats: "http://baseball.zengm.com/l/2/player/2281"
+  },
+  {
+    name: "Craig Rodriguez",
+    yearsPlayed: 16,
+    team: "Phoenix Vultures",
+    imgURL: "",
+    position: "1B",
+    worldSeriesWins: 0,
+    mvp: 0,
+    stats: "http://baseball.zengm.com/l/2/player/5832"
+  },
+  {
+    name: "Matthew Christopher",
+    yearsPlayed: 16,
+    team: "Chicago Whirlwinds",
+    imgURL: "",
+    position: "1B",
+    worldSeriesWins: 0,
+    mvp: 1,
+    stats: "http://baseball.zengm.com/l/2/player/5178"
+  },
+  {
+    name: "Jesse Hamilton",
+    yearsPlayed: 14,
+    team: "Phoenix Vultures",
+    imgURL: "",
+    position: "3B",
+    worldSeriesWins: 0,
+    mvp: 0,
+    stats: "http://baseball.zengm.com/l/2/player/2915"
+  },
+  {
+    name: "Dwight Kerr",
+    yearsPlayed: 15,
+    team: "Houston Apollos",
+    imgURL: "",
+    position: "3B",
+    worldSeriesWins: 1,
+    mvp: 0,
+    stats: "http://baseball.zengm.com/l/2/player/6008"
+  },
+  {
+    name: "Richard Evans",
+    yearsPlayed: 17,
+    team: "San Francisco Venture Capitalists",
+    imgURL: "",
+    position: "1B",
+    worldSeriesWins: 4,
+    mvp: 1,
+    stats: "http://baseball.zengm.com/l/2/player/6848"
+  },
+  {
+    name: "Jason Janes",
+    yearsPlayed: 11,
+    team: "Phoenix Vultures",
+    imgURL: "",
+    position: "1B",
+    worldSeriesWins: 0,
+    mvp: 3,
+    stats: "http://baseball.zengm.com/l/2/player/7017"
+  },
+  {
+    name: "Alan Fleck",
+    yearsPlayed: 16,
+    team: "San Francisco Venture Capitalists",
+    imgURL: "",
+    position: "LF",
+    worldSeriesWins: 10,
+    mvp: 0,
+    stats: "http://baseball.zengm.com/l/2/player/5559"
+  },
+  {
+    name: "Jacob Sabatini",
+    yearsPlayed: 14,
+    team: "San Francisco Venture Capitalists",
+    imgURL: "",
+    position: "LF",
+    worldSeriesWins: 14,
+    mvp: 0,
+    stats: "http://baseball.zengm.com/l/2/player/2187"
+  }
+];
 
 /*
  * JSON API Endpoints
@@ -47,16 +159,23 @@ app.get('/api', function apiIndex(req, res) {
   // It would be seriously overkill to save any of this to your database.
   // But you should change almost every line of this response.
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
+    // woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentationUrl: "https://github.com/ibi8588/express-personal-api/blob/master/README.md", // github readme
+    baseUrl: "https://protected-retreat-76331.herokuapp.com/", // heroku page
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "Data about me"}, // links to portfolio page
+      {method: "GET", path: "/api/players", description: "Index of all baseball players"}, // Gets all baseball players
+      {method: "POST", path: "/api/players/:id", description: "create a baseball player"},
+      {method: "PUT", path: "/api/players/:id", description: "edit a player"},
+      {method: "DELETE", path: "/api/players/:id", description: "deleta a player"}
     ]
   })
+});
+
+app.get('/api/players', function(req, res){
+  res.json(players_list)
 });
 
 /**********
